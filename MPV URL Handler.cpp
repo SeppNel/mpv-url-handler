c9d61cb60639ece1cf67186f1b1afbbddcbf4a86
SeppNel <josenova1000@gmail.com>
2024-01-24 18:35:26 +0100

    Use encoded urls

--- MPV URL Handler.cpp
+++ MPV URL Handler.cpp
@@ -4,6 +4,7 @@
 #include <yaml-cpp/yaml.h>
 #include <filesystem>
 #include <vector>
+#include <regex>
 
 using namespace m4x1m1l14n;
 using std::cout;
@@ -131,6 +132,33 @@ void argumentHandler(int argc, char* argv[]) {
 	}
 }
 
+std::string decodeURIComponent(std::string encoded) { //https://gist.github.com/arthurafarias/56fec2cd49a32f374c02d1df2b6c350f
+
+	std::string decoded = encoded;
+	std::smatch sm;
+	std::string haystack;
+
+	int dynamicLength = decoded.size() - 2;
+
+	if (decoded.size() < 3) return decoded;
+
+	for (int i = 0; i < dynamicLength; i++) {
+
+		haystack = decoded.substr(i, 3);
+
+		if (std::regex_match(haystack, sm, std::regex("%[0-9A-F]{2}"))) {
+			haystack = haystack.replace(0, 1, "0x");
+			std::string rc = { (char)std::stoi(haystack, nullptr, 16) };
+			decoded = decoded.replace(decoded.begin() + i, decoded.begin() + i + 3, rc);
+		}
+
+		dynamicLength = decoded.size() - 2;
+
+	}
+
+	return decoded;
+}
+
 int main(int argc, char* argv[]) {
 	if (!keyExists()) {
 		createHandler();
@@ -158,6 +186,7 @@ int main(int argc, char* argv[]) {
 
 	string mpvPath = config["mpvPath"].as<string>();
 	string fileToPlay(argv[1]);
+	fileToPlay = decodeURIComponent(fileToPlay);
 	fileToPlay = fileToPlay.substr(6, fileToPlay.npos);
 
 	string protocol = fileToPlay.substr(0, fileToPlay.find(':'));
